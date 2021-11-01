a78c21ab3750b5bb63a6d620e60b9bc02d5cc962
George Agyemang <george.polymorph@gmail.com>
2021-11-01 15:34:20 +0000

    responsiveness

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -693,7 +693,7 @@
         e = e || window.event;
         var charCode = (typeof e.which == "undefined") ? e.keyCode : e.which;
         var charStr = String.fromCharCode(charCode);
-        if (!charStr.match(/^[0-9]+$/))
+        if (!charStr.match(^[-+()][0-9][-+()0-9]$))
             e.preventDefault();
     }
 </script>
